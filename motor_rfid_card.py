029b51d4e07e6279a7211cc4373cad8b8a96faff
Adiyatma Koto <adiyatma.padang@gmail.com>
2021-08-17 23:41:52 +0700

    adit commit rfid plus servo

---
+++ motor_rfid_card.py
@@ -0,0 +1,37 @@
+import RPi.GPIO as GPIO
+import time
+from mfrc522 import SimpleMFRC522
+
+reader = SimpleMFRC522()
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(13,GPIO.OUT)
+
+servo = GPIO.PWM(13,50)
+servo.start(0)
+time.sleep(2)
+
+try:
+    id,text = reader.read()
+    print(id)
+    print(text)
+    servo.start(0)
+    time.sleep(0.5)
+    servo.ChangeDutyCycle(7)
+    time.sleep(0.5)
+    time.sleep(1)
+    servo.ChangeDutyCycle(0)
+finally:
+    servo.stop()
+    GPIO.cleanup()
+
+
+
+servo.ChangeDutyCycle(2)
+time.sleep(1)
+servo.ChangeDutyCycle(0)
+
+servo.stop()
+
+print("finish")
