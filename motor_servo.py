00bdbf73c00a76cbbca188b3cb761ae6808a3cb6
Adiyatma Koto <adiyatma.padang@gmail.com>
2021-08-17 23:07:53 +0700

    adit commit repair servo

--- motor_servo.py
+++ motor_servo.py
@@ -7,8 +7,19 @@ GPIO.setup(11,GPIO.OUT)
 
 servo = GPIO.PWM(11,50)
 servo.start(0)
-time.sleep(1)
+time.sleep(2)
 
-servo.ChangeDutyCycle(7)
+print("putar 90 derajat")
+duty = 2
+while duty<=7:
+    servo.ChangeDutyCylce(duty)
+    time.sleep(0.5)
+    duty=duty+1
+
+servo.ChangeDutyCycle(2)
 time.sleep(1)
 servo.ChangeDutyCycle(0)
+
+servo.stop()
+
+print("finish")
